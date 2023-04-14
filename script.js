a67f5e5e32d6a86e186565d12c0b742af3fc5f72
DaamHaam <63154077+DaamHaam@users.noreply.github.com>
2023-04-14 12:31:09 +0200

    item barré

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ items.forEach(item => {
     // d'ajouter ou de supprimer une classe CSS d'un élément HTML en fonction de son état.
     // La classe "completed" n'est pas une classe CSS prédéfinie, elle est simplement utilisée dans notre exemple pour donner un nom à la classe que nous ajoutons ou supprimons des éléments <li> lorsqu'ils sont cliqués. Vous pouvez utiliser un autre nom pour cette classe si vous le souhaitez, tant que vous utilisez le même nom dans le code HTML, le code CSS et le code JavaScript
     item.classList.toggle('completed');
+    console.log("ITEM barré !");
+
   });
 });
 
