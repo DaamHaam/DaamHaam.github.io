942c417c5298b88e238a36357643a4a9774a85a4
DaamHaam <63154077+DaamHaam@users.noreply.github.com>
2023-04-25 16:23:37 +0200

    change app

---
+++ page2/app.js
@@ -0,0 +1,56 @@
+let score = 0;
+let timeLeft = 30;
+let timer;
+
+function generateOperation() {
+    let num1 = Math.floor(Math.random() * 20) + 1;
+    let num2 = Math.floor(Math.random() * 20) + 1;
+    document.getElementById('operation').innerText = `${num1} + ${num2}`;
+}
+
+function checkAnswer() {
+    let answer = parseInt(document.getElementById('answer').value);
+    let operation = document.getElementById('operation').innerText.split(' ');
+    let correctAnswer = parseInt(operation[0]) + parseInt(operation[2]);
+
+    if (answer === correctAnswer) {
+        score++;
+        document.getElementById('score').innerText = `Score: ${score}`;
+    }
+    generateOperation();
+    document.getElementById('answer').value = '';
+}
+
+function countdown() {
+    timeLeft--;
+    document.getElementById('timer').innerText = `Temps restant: ${timeLeft}s`;
+
+    if (timeLeft <= 0) {
+        clearInterval(timer);
+        document.getElementById('submit').disabled = true;
+        alert(`Temps écoulé ! Votre score est de ${score}`);
+    }
+}
+
+function restartGame() {
+  clearInterval(timer);
+  score = 0;
+  timeLeft = 30;
+  document.getElementById('score').innerText = 'Score: 0';
+  document.getElementById('timer').innerText = 'Temps restant: 30s';
+  document.getElementById('submit').disabled = false;
+  generateOperation();
+  timer = setInterval(countdown, 1000);
+}
+
+document.getElementById('submit').addEventListener('click', checkAnswer);
+
+// Initial setup
+document.getElementById('answer').addEventListener('keyup', function(event) {
+  if (event.key === 'Enter') {
+      event.preventDefault();
+      checkAnswer();
+  }
+});
+generateOperation();
+timer = setInterval(countdown, 1000);
--- script.js
+++ script.js
@@ -1,40 +1,13 @@
-// Sélectionnez tous les éléments <li> de la liste
-const items = document.querySelectorAll('ul li');
+document.addEventListener("DOMContentLoaded", function () {
+    const buttonsContainer = document.getElementById("buttons-container");
+    const totalPages = 3;
 
-// Ajoute un gestionnaire d'événement pour chaque élément <li>
-items.forEach(item => {
-  item.addEventListener('click', () => {
-    // Inverse la classe "completed" de l'élément <li>
-    // la méthode toggle() est une méthode JavaScript qui permet 
-    // d'ajouter ou de supprimer une classe CSS d'un élément HTML en fonction de son état.
-    // La classe "completed" n'est pas une classe CSS prédéfinie, elle est simplement utilisée dans notre exemple pour donner un nom à la classe que nous ajoutons ou supprimons des éléments <li> lorsqu'ils sont cliqués. Vous pouvez utiliser un autre nom pour cette classe si vous le souhaitez, tant que vous utilisez le même nom dans le code HTML, le code CSS et le code JavaScript
-    item.classList.toggle('completed');
-    console.log("ITEM barré !");
-
-  });
+    for (let i = 1; i <= totalPages; i++) {
+        const button = document.createElement("button");
+        button.textContent = i;
+        button.addEventListener("click", function () {
+            window.location.href = `page${i}/page${i}.html`;
+        });
+        buttonsContainer.appendChild(button);
+    }
 });
-
-// Définition d'un tableau de tâches à faire
-const tasks = ['Acheter du lait', 'Appeler le plombier', 'Faire du sport'];
-
-// Stockage du tableau de tâches dans le sessionStorage sous la clé 'tasks'
-sessionStorage.setItem('tasks', JSON.stringify(tasks));
-
-// Récupération du tableau depuis le sessionStorage
-const storedTasks = JSON.parse(sessionStorage.getItem('tasks'));
-
-// Affichage du tableau dans la console
-console.log("HEY !");
-console.log("stored : " + storedTasks);
-
-
-
-
-// TESTER local storage
-// installer node.js et express pour projet séparément du projet dans github ??
-// contourner CORS pour localstorage dans PWA ? cache de service ? websql ?
-// tester console log dans le navigateur ?
-// Firebase pour pwa tuto https://firebase.google.com/docs/web/pwa?hl=fr
-
-
-// voir web app install banner pr faciliter l'installation de l'app
